187e9eb708714f051b6f61f8a49d73d609be2cc0
rwblickhan <rwblickhan@gmail.com>
2017-08-16 19:48:50 -0700

    Add glm as a dependency

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+mkdir deps && cd deps
 git clone https://github.com/JoshuaBrookover/bgfx.cmake.git && cd bgfx.cmake && git submodule init && git submodule update
+cd .. && git clone https://github.com/g-truc/glm.git
 cd .. && mkdir build && cd build && cmake .. && make
